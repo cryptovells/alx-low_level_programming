91218bac24fe7f0f810ec70f5aac13e0470acb34
cryptovells <precovellameribat@gmail.com>
2022-09-19 08:58:05 -0700

    C program

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/*
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z', ch != 'e', ch != 'q')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
+}
