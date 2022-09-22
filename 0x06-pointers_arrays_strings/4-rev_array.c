48b312f4b832db27fac31f1eb3873f3dbf611ba6
cryptovells <precovellameribat@gmail.com>
2022-09-22 16:14:27 -0700

    C program

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * reverse_array - function that reverses array of n integers
+ *
+ * @a: array
+ *
+ * @n: number of arrays
+ *
+ */
+
+void reverse_array(int *a, int n)
+
+{
+		int x = 0, z;
+
+		n = n - 1;
+		while (x <= n)
+		{
+		z = a[x];
+		a[x++] = a[n];
+		a[n--] = z;
+		}
+}
---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * cap_string - function that capitalizes all words of a string
+ *
+ * @str: string parameter
+ *
+ * Return: capitalised string
+ *
+ */
+
+char *cap_string(char *str)
+
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+	if (str[0] <= 122 && str[0] >= 97)
+	{
+	str[0] = str[0] - 32;
+	}
+	if (str[i] == 32 || str[i] == 46 || str[i] == '\t' ||
+	str[i] == '\n' || str[i] == 44 || str[i] == 59 ||
+	str[i] == '!' || str[i] == '?' || str[i] == '(' ||
+	str[i] == ')' || str[i] == '{' || str[i] == '}')
+	{
+	if (str[i + 1] <= 122 && str[i + 1] >= 97)
+	{
+	str[i + 1] = str[i + 1] - 32;
+	}
+	}
+	i++;
+	}
+	return (str);
+}
