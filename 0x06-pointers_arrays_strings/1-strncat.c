5c1d1a35d5f07063144569af6abfa992a0a47a7f
cryptovells <precovellameribat@gmail.com>
2022-09-22 15:21:46 -0700

    C program

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strncat - function that concatenates string
+ *
+ * @dest: string to append to
+ *
+ * @src: string to append
+ *
+ * @n: number of bytes
+ *
+ * Return: concatenated string
+ *
+ */
+
+char *_strncat(char *dest, char *src, int n)
+
+{
+	int i, j, m;
+
+	for (m = 0; dest[m] != '\0'; m++)
+	{
+	}
+	for (i = m, j = 0; j < n; i++, j++)
+	{
+		dest[i] = src[j];
+		if (src[j] == '\0')
+		j = n;
+	}
+	return (dest);
+}
