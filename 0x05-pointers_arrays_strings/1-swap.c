4edf78cd587a2890d5d878708298b7c4d5ed0f93
cryptovells <precovellameribat@gmail.com>
2022-09-23 08:58:19 -0700

    C program

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * swap_int - function that swaps the values of two integers
+ *
+ * @a: first parameter
+ *
+ * @b: second parameter
+ *
+ */
+
+void swap_int(int *a, int *b)
+
+{
+	int c = *a;
+
+	int d = *b;
+
+	*a = d;
+	*b = c;
+}
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - function to print a string
+ *
+ * @str: the string parameter
+ *
+ */
+
+void _puts(char *str)
+
+{
+	puts(str);
+}
